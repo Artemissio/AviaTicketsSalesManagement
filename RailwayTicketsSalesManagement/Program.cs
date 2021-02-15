115f008ef5b62a81adc8cdf4d60220017b0d18c4
Artemissio <artemissterio@gmail.com>
2021-02-15 15:10:18 +0200

    Created project
    Created project

---
+++ RailwayTicketsSalesManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RailwayTicketsSalesManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
