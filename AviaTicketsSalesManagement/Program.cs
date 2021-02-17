436e2f2853ab665bcb747446818de670e204f3db
Artemissio <artemissterio@gmail.com>
2021-02-17 10:29:40 +0200

    Release lab1
    Release lab1

--- AviaTicketsSalesManagement/Program.cs
+++ AviaTicketsSalesManagement/Program.cs
@@ -7,7 +7,9 @@ namespace RailwayTicketsSalesManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\t=== Avia Tickets Booking System ===");
+            Console.WriteLine("Made by students of IA-71");
+            Console.WriteLine("Artem Tarasenko, Julia Koval, Maxim Terentiev");
 
             Console.ReadKey();
         }
